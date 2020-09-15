1701e9d86a23a2d0ff1909aaab06ed53a3a26e05
Trevor Karjanis <2351292+TrevorKarjanis@users.noreply.github.com>
2020-09-15 18:36:58 -0400

    Add the no-console rule.

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -6,5 +6,9 @@ module.exports = {
         unique exceptions where this rule can be disabled with an inline comment.
     */
     'no-await-in-loop': 'error',
+    /*
+        Console logs should not be exposed in production environments.
+    */
+    'no-console': 'error',
   },
 };
