a16d7f74d0fbce6643172e694558ad8d03e95c71
Trevor Karjanis <2351292+TrevorKarjanis@users.noreply.github.com>
2020-09-15 18:15:33 -0400

    Add the no-await-in-loop rule.

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,3 +1,10 @@
 module.exports = {
   extends: 'airbnb-base',
+  rules: {
+    /*
+        Errors from this rule will identify issues more often than not. However, there may be
+        unique exceptions where this rule can be disabled with an inline comment.
+    */
+    'no-await-in-loop': 'error',
+  },
 };
