7f6893811af1596efcffd6855b4f32e874f546ae
Trevor Karjanis <2351292+TrevorKarjanis@users.noreply.github.com>
2020-09-16 14:29:38 -0400

    Add the arrow-parens, eol-last, and indent rules.

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,23 +1,36 @@
 module.exports = {
-  extends: 'airbnb-base',
-  rules: {
-    /*
-        This rule is controversial and is defined such that trailing commas are not required but
-        may be used if preferred.
-    */
-    'comma-dangle': ['error', 'only-multiline'],
-    /*
-        Errors from this rule will identify issues more often than not. However, there may be
-        unique exceptions where this rule can be disabled with an inline comment.
-    */
-    'no-await-in-loop': 'error',
-    /*
-        Console logs should not be exposed in production environments.
-    */
-    'no-console': 'error',
-    /*
-        Migrating from var is not trivial, so there may be exceptions for older projects.
-    */
-    'no-var': 'error',
-  },
+    extends: 'airbnb-base',
+    rules: {
+        /*
+            Omit arrow function parenthesis where they are not required to improve readability.
+        */
+        'arrow-parens': ['error', 'as-needed'],
+        /*
+            This rule is controversial and is defined such that trailing commas are not required but
+            may be used if preferred.
+        */
+        'comma-dangle': ['error', 'only-multiline'],
+        /*
+            Do not require a newline at the end of every file in favor of reducing the effort to
+            satisfy the rule over the proposed advantages on UNIX and with concatenation.
+        */
+        'eol-last': 'off',
+        /*
+            This indentation size is used to promote consistency.
+        */
+        indent: ['error', 4],
+        /*
+            Errors from this rule will identify issues more often than not. However, there may be
+            unique exceptions where this rule can be disabled with an inline comment.
+        */
+        'no-await-in-loop': 'error',
+        /*
+            Console logs should not be exposed in production environments.
+        */
+        'no-console': 'error',
+        /*
+            Migrating from var is not trivial, so there may be exceptions for older projects.
+        */
+        'no-var': 'error',
+    }
 };
