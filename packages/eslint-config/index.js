fa26a388f82a66317b733f5371b7ff9c7ad2997c
Trevor Karjanis <2351292+TrevorKarjanis@users.noreply.github.com>
2020-09-16 12:20:28 -0400

    Add the comma-dangle and no-var rules.

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -2,6 +2,11 @@ module.exports = {
   extends: 'airbnb-base',
   rules: {
     /*
+        This rule is controversial and is defined such that trailing commas are not required but
+        may be used if preferred.
+    */
+    'comma-dangle': ['error', 'only-multiline'],
+    /*
         Errors from this rule will identify issues more often than not. However, there may be
         unique exceptions where this rule can be disabled with an inline comment.
     */
@@ -10,5 +15,9 @@ module.exports = {
         Console logs should not be exposed in production environments.
     */
     'no-console': 'error',
+    /*
+        Migrating from var is not trivial, so there may be exceptions for older projects.
+    */
+    'no-var': 'error',
   },
 };
