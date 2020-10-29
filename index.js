b0526d688daccf1f1cd9306665c82364197063e5
Trevor Karjanis <2351292+TrevorKarjanis@users.noreply.github.com>
2020-10-29 10:03:01 -0400

    Add resolved rules and comments. (#4)
    * Add the no-restricted-syntax rule.
    
    * Add the object-curly-newline rule.
    
    * Add the no-mixed-operators rule.
    
    * Add the import/extensions and no-use-before-define rules.
    
    * Update the space-before-function-paren rule.

--- index.js
+++ index.js
@@ -25,6 +25,18 @@ module.exports = {
         'func-names': ['error', 'as-needed'],
 
         /*
+            There may be exceptions where it is appropriate to disable this rule for projects that
+            require ES6 compliant module imports. It is understood that the compliance standards
+            may change and that this rule may need to be reevaluated.
+        */
+        'import/extensions': ['error', 'ignorePackages', {
+            js: 'never',
+            mjs: 'never',
+            jsx: 'never',
+            ts: 'never'
+        }],
+
+        /*
             This indentation size is used to promote consistency.
         */
         indent: ['error', 4],
@@ -63,15 +75,66 @@ module.exports = {
         'no-loop-func': 'error',
 
         /*
+            Enforce parenthesis in cases of mixed precedence.
+        */
+        'no-mixed-operators': ['error', {
+            groups: [
+                ['%', '**'],
+                ['%', '+'],
+                ['%', '-'],
+                ['%', '*'],
+                ['%', '/'],
+                ['/', '*'],
+                ['&', '|', '<<', '>>', '>>>'],
+                ['==', '!=', '===', '!=='],
+                ['&&', '||'],
+            ],
+            allowSamePrecedence: true
+        }],
+
+        /*
             This rule defends against prototype pollution vulnerabilities that exploitable with
             XSS.
         */
         'no-prototype-builtins': 'error',
 
         /*
+            Higher-order function composition, like Array.forEach(), is preferred for iterative
+            loops. For...in statements can be used when performance is a consideration as long as
+            guard-for-in is enabled. For...of statements can be used when break is required for
+            control flow or the iteration produces side-effects.
+        */
+        'no-restricted-syntax': [
+            'error', {
+                selector: 'LabeledStatement',
+                message: 'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
+            }, {
+                selector: 'WithStatement',
+                message: '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
+            }],
+
+        /*
             Migrating from var is not trivial, so there may be exceptions for older projects.
         */
         'no-var': 'error',
+
+        /*
+            Defining classes and variables before their use can cause errors. However, placing
+            function declarations at the end of a file is a common programming practice for
+            readability.
+        */
+        'no-use-before-define': ['error', { functions: false, classes: true, variables: true }],
+
+        /*
+            Allow single line object expressions and patterns, but ensure a reasonable line length.
+            All expressions, patterns, and declarations should be consistent.
+        */
+        'object-curly-newline': ['error', {
+            ObjectExpression: { minProperties: 6, multiline: true, consistent: true },
+            ObjectPattern: { minProperties: 6, multiline: true, consistent: true },
+            ImportDeclaration: { consistent: true },
+            ExportDeclaration: { consistent: true }
+        }],
 
         /*
             This configuration already supports the JSDoc syntax. Add additional syntax as line or
@@ -83,12 +146,17 @@ module.exports = {
             Enforcing this rule improves code readability by making its appearance more consistent
             across codebases. Disabling or changing the value of this rule is discouraged.
 
-            We elect not to include a space before function parentheses. There are advocates in the
-            external JavaScript community and within NI for both this configuration and
-            others that include a space. Each of those opinions have valid merits. The primary
-            justification is that the syntax without a space will be more familiar to the many NI
-            web developers with experience in other languages (e.g. C, C++, C#, and Python).
+            We elect not to include a space before function parentheses except for anonymous
+            functions. There are advocates in the external JavaScript community and within NI for
+            both this configuration and others that include a space. Each of those opinions have
+            valid merits. The primary justification is that the syntax without a space will be more
+            familiar to the many NI web developers with experience in other languages (e.g. C, C++,
+            C#, and Python).
         */
-        'space-before-function-paren': ['error', 'never']
+        'space-before-function-paren': ['error', {
+            anonymous: 'always',
+            named: 'never',
+            asyncArrow: 'always'
+        }]
     },
 };
