33897ed15befbe527180de408710e352d2edd590
rajsite <rajsite@users.noreply.github.com>
2021-01-20 12:35:46 -0600

    Disable typescript requiring type checking rules

--- typescript-requiring-type-checking.js
+++ typescript-requiring-type-checking.js
@@ -15,24 +15,24 @@ module.exports = {
         /*
             TypeScript rules outside of the recommended configuration
         */
-        '@typescript-eslint/naming-convention': 'error',
-        '@typescript-eslint/no-base-to-string': 'error',
-        '@typescript-eslint/no-confusing-void-expression': 'error',
-        '@typescript-eslint/no-unnecessary-boolean-literal-compare': 'error',
-        '@typescript-eslint/no-unnecessary-condition': 'error',
-        '@typescript-eslint/no-unnecessary-qualifier': 'error',
-        '@typescript-eslint/no-unnecessary-type-arguments': 'error',
+        // '@typescript-eslint/naming-convention': 'error',
+        // '@typescript-eslint/no-base-to-string': 'error',
+        // '@typescript-eslint/no-confusing-void-expression': 'error',
+        // '@typescript-eslint/no-unnecessary-boolean-literal-compare': 'error',
+        // '@typescript-eslint/no-unnecessary-condition': 'error',
+        // '@typescript-eslint/no-unnecessary-qualifier': 'error',
+        // '@typescript-eslint/no-unnecessary-type-arguments': 'error',
         // Available in newer typescript-eslint version
         // '@typescript-eslint/non-nullable-type-assertion-style': 'error',
-        '@typescript-eslint/prefer-includes': 'error',
-        '@typescript-eslint/prefer-nullish-coalescing': 'error',
-        '@typescript-eslint/prefer-readonly': 'error',
-        '@typescript-eslint/prefer-readonly-parameter-types': 'error',
-        '@typescript-eslint/prefer-reduce-type-parameter': 'error',
-        '@typescript-eslint/prefer-string-starts-ends-with': 'error',
-        '@typescript-eslint/promise-function-async': 'error',
-        '@typescript-eslint/require-array-sort-compare': 'error',
-        '@typescript-eslint/strict-boolean-expressions': 'error',
-        '@typescript-eslint/switch-exhaustiveness-check': 'error'
+        // '@typescript-eslint/prefer-includes': 'error',
+        // '@typescript-eslint/prefer-nullish-coalescing': 'error',
+        // '@typescript-eslint/prefer-readonly': 'error',
+        // '@typescript-eslint/prefer-readonly-parameter-types': 'error',
+        // '@typescript-eslint/prefer-reduce-type-parameter': 'error',
+        // '@typescript-eslint/prefer-string-starts-ends-with': 'error',
+        // '@typescript-eslint/promise-function-async': 'error',
+        // '@typescript-eslint/require-array-sort-compare': 'error',
+        // '@typescript-eslint/strict-boolean-expressions': 'error',
+        // '@typescript-eslint/switch-exhaustiveness-check': 'error'
     }
 };
