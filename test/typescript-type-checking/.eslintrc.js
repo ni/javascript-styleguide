b211ff0cee39087284212225fc660379e9173077
Trevor Karjanis <2351292+TrevorKarjanis@users.noreply.github.com>
2021-05-28 11:16:05 -0400

    Add the Angular configuration to the evaluated rules print utility.

--- test/typescript-type-checking/.eslintrc.js
+++ test/typescript-type-checking/.eslintrc.js
@@ -4,6 +4,8 @@ module.exports = {
     ignorePatterns: ['*.js'],
     parserOptions: {
         project: ['./tsconfig.json'],
+        // Resolve the project relative to this file's directory.
+        // https://stackoverflow.com/questions/64933543/parsing-error-cannot-read-file-tsconfig-json-eslint
         tsconfigRootDir: __dirname,
     },
 };
--- tools/print-evaluated-rules.js
+++ tools/print-evaluated-rules.js
@@ -9,17 +9,21 @@ const { ESLint } = require('eslint');
     const configEslint = await eslint.calculateConfigForFile(`${__dirname}/../test/eslint/index.js`);
     const configTypescript = await eslint.calculateConfigForFile(`${__dirname}/../test/typescript/index.js`);
     const configTypescriptTypechecked = await eslint.calculateConfigForFile(`${__dirname}/../test/typescript-type-checking/index.js`);
+    const configAngular = await eslint.calculateConfigForFile(`${__dirname}/../test/angular/index.ts`);
 
     global.console.log('-------- Evaluated rules:');
-    global.console.log('Evaluated eslint rules:');
+    global.console.log('Evaluated ESLint rules:');
     global.console.log(JSON.stringify(configEslint, undefined, 4));
     global.console.log('Evaluated TypeScript rules:');
     global.console.log(JSON.stringify(configTypescript, undefined, 4));
     global.console.log('Evaluated TypeScript type checking rules:');
     global.console.log(JSON.stringify(configTypescriptTypechecked, undefined, 4));
+    global.console.log('Evaluated Angular rules:');
+    global.console.log(JSON.stringify(configAngular, undefined, 4));
 
     global.console.log('-------- Audits:');
-    global.console.log(`Evaluated eslint rules has warn?: ${hasWarn(configEslint)}`);
+    global.console.log(`Evaluated ESLint rules has warn?: ${hasWarn(configEslint)}`);
     global.console.log(`Evaluated TypeScript rules has warn?: ${hasWarn(configTypescript)}`);
     global.console.log(`Evaluated TypeScript type checking rules has warn?: ${hasWarn(configTypescriptTypechecked)}`);
+    global.console.log(`Evaluated Angular rules has warn?: ${hasWarn(configAngular)}`);
 })();
