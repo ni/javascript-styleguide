5882a31033aaf7bac5626b7380b41bce084f1ebe
Jesse Attas <jattasNI@users.noreply.github.com>
2022-10-04 11:03:57 -0500

    Remove @ni/beachball-lock-update and update version of beachball (#109)
    # Justification
    
    We were using the `@ni/beachball-lock-update` package to work around [a
    bug](https://github.com/microsoft/beachball/issues/525) with how
    beachball modifies `package-lock.json` when publishing. That bug has
    been addressed in more recent versions of beachball so we can stop using
    the workaround.
    
    This moves us towards fixing [this Nimble
    issue](https://github.com/ni/nimble/issues/601).
    
    # Implementation
    
    1. Remove `@ni/beachball-lock-update` from `package.json` and
    `beachball.config.js`.
    2. Install the latest version of `beachball`
    
    # Testing
    
    1. We've been running with this setup in Nimble for a few months and
    it's worked well
    2. I verified that basic beachball commands still work
    3. I'll monitor the publishing pipeline as future changes go in. I
    considered making a trivial edit to a pakckage to trigger a publish but
    that seems pretty noisy now that our apps uptake new versions every
    night.

--- beachball.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-const lockPath = path.resolve(__dirname, './package-lock.json');
-const { createPostbump } = require('@ni/beachball-lock-update');
-
-module.exports = {
-    hooks: {
-        postbump: createPostbump(lockPath)
-    }
-};
