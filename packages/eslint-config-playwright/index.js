689bdf507644e2f33358819d623934e528099a77
Jesse Attas <jattasNI@users.noreply.github.com>
2023-01-26 15:54:40 -0600

    New playwright package

---
+++ packages/eslint-config-playwright/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+    extends: [
+        '@ni/eslint-config-typescript'
+    ],
+    rules: {
+    },
+    overrides: [
+    ]
+};
---
+++ packages/eslint-config-playwright/requiring-type-checking.js
@@ -0,0 +1,10 @@
+module.exports = {
+    extends: [
+        '@ni/eslint-config-typescript/requiring-type-checking'
+    ],
+    parser: '@typescript-eslint/parser',
+    overrides: [
+        {
+        },
+    ]
+};
