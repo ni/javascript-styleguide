6044ee8d4ed4d65d3702210cffca5ca73621f206
Jesse Attas <jattasNI@users.noreply.github.com>
2023-02-23 15:18:40 -0600

    Rule names

--- packages/eslint-config-playwright/tools/print-available-rules.js
+++ packages/eslint-config-playwright/tools/print-available-rules.js
@@ -2,7 +2,7 @@ const plugin = require('eslint-plugin-playwright');
 
 const isTrue = val => val !== undefined && val !== false;
 const recommended = rule => isTrue(rule.meta.docs.recommended);
-const print = (keys, prefix = 'eslint-plugin-playwright') => {
+const print = (keys, prefix = 'playwright') => {
     const results = {};
     keys.forEach(key => { results[`${prefix}/${key}`] = ''; });
     global.console.log(JSON.stringify(results, null, 4));
@@ -12,7 +12,7 @@ const sortedRules = Object.keys(plugin.rules).sort();
 const playwright = sortedRules.filter(key => !recommended(plugin.rules[key]));
 const playwrightRecommended = sortedRules.filter(key => recommended(plugin.rules[key]));
 
-global.console.log('playwright Recommended:');
+global.console.log('Playwright Recommended:');
 print(playwrightRecommended);
-global.console.log('Remaining playwright Rules:');
+global.console.log('Remaining Playwright Rules:');
 print(playwright);
