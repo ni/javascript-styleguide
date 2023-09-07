2266dea2452e6daa915f8cd721e36f23f67491d1
Carson Moore <carson.moore@ni.com>
2023-09-07 15:29:45 -0500

    Remove dep and copy rules

--- packages/eslint-config-javascript/index.js
+++ packages/eslint-config-javascript/index.js
@@ -1,6 +1,13 @@
 module.exports = {
     extends: [
-        'airbnb-base'
+        './rules/best-practices',
+        './rules/errors',
+        './rules/node',
+        './rules/style',
+        './rules/variables',
+        './rules/es6',
+        './rules/imports',
+        './rules/strict'
     ],
     rules: {
         /*
