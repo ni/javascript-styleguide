170576b45dcce629dba70e3298baa667c0c4a84d
Mert Akinc <7282195+m-akinc@users.noreply.github.com>
2024-03-19 16:49:41 -0500

    Disable interactive-supports-focus (by default)

--- packages/eslint-config-angular/template.js
+++ packages/eslint-config-angular/template.js
@@ -17,6 +17,7 @@ module.exports = {
         '@angular-eslint/template/alt-text': 'off',
         '@angular-eslint/template/click-events-have-key-events': 'off',
         '@angular-eslint/template/elements-content': 'off',
+        '@angular-eslint/template/interactive-supports-focus': 'off',
         '@angular-eslint/template/label-has-associated-control': 'off',
         '@angular-eslint/template/mouse-events-have-key-events': 'off',
         '@angular-eslint/template/no-autofocus': 'off',
