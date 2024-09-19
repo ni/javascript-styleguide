fc777e89d807139f0abc4d0eb3577f71ce994f4f
Jesse Attas <jattasNI@users.noreply.github.com>
2024-09-19 16:25:46 -0500

    Restore / update comment about unbound-method

--- packages/eslint-config-angular/requiring-type-checking.js
+++ packages/eslint-config-angular/requiring-type-checking.js
@@ -7,6 +7,10 @@ module.exports = {
         {
             files: ['*.spec.ts'],
             rules: {
+                /*
+                    Spies used by Angular application tests result in a significant number of
+                    unbound methods so this rule is disabled for test specs in Angular projects.
+                */
                 '@typescript-eslint/unbound-method': 'off',
             }
         },
