dbe3a88deb1cd2e90c2f5a3b19be21a96efaefe9
Mert Akinc <7282195+m-akinc@users.noreply.github.com>
2025-01-08 14:40:25 -0600

    Fix issue where error is reported about missing ts parser project config

--- tests/playwright/.eslintrc.js
+++ tests/playwright/.eslintrc.js
@@ -1,11 +1,13 @@
 module.exports = {
-    extends: '@ni/eslint-config-playwright',
     root: true,
+    ignorePatterns: ['*.js'],
     overrides: [
         {
-            files: [
-                '*.ts',
-            ],
+            extends: '@ni/eslint-config-playwright',
+            files: ['*.ts'],
+            parserOptions: {
+                project: './tsconfig.json'
+            },
             rules: {
                 // In this example app, all files are allowed to refer to playwright packages from devDependencies
                 'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
--- tests/typescript/.eslintrc.js
+++ tests/typescript/.eslintrc.js
@@ -1,4 +1,17 @@
 module.exports = {
-    extends: '@ni/eslint-config-typescript',
-    root: true
+    ignorePatterns: ['*.js'],
+    root: true,
+    overrides: [
+        {
+            extends: '@ni/eslint-config-typescript',
+            files: ['*.ts'],
+            parserOptions: {
+                project: './tsconfig.json'
+            },
+            rules: {
+                // In this example app, all files are allowed to refer to playwright packages from devDependencies
+                'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+            }
+        }
+    ]
 };
