f22f19ffe9ff8a0c4feabeb710aa4ad913a26ad6
Jesse Attas <jattasNI@users.noreply.github.com>
2025-09-22 12:31:51 -0500

    no-unresolved

--- packages/eslint-config-typescript/eslint.config.js
+++ packages/eslint-config-typescript/eslint.config.js
@@ -9,7 +9,16 @@ export default [
         files: ['**/*.js'],
         rules: {
             'import/extensions': 'off',
-            'import/no-unresolved': 'off',
+            // eslint-plugin-import doesn't know how to resolve entry points in packages
+            // that use modern export maps in package.json.
+            // https://github.com/typescript-eslint/typescript-eslint/issues/7565
+            // https://github.com/import-js/eslint-plugin-import/issues/2703
+            'import/no-unresolved': [
+                'error',
+                {
+                    ignore: ['typescript-eslint']
+                }
+            ],
         },
     },
 ];
