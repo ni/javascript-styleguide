1de9e262278f42822d6a55af17776692cd466147
gokulprasanth-ni <gokulprasanth.ravi@emerson.com>
2025-09-23 11:04:01 +0530

    clean up
    Signed-off-by: gokulprasanth-ni <gokulprasanth.ravi@emerson.com>

--- packages/eslint-config-playwright/eslint.config.js
+++ packages/eslint-config-playwright/eslint.config.js
@@ -4,9 +4,5 @@ export default [
     ...javascriptConfig,
     {
         ignores: ['eslint.config.js'],
-    },
-    {
-        rules: {
-        },
-    },
+    }
 ];
--- tests/print-evaluated-rules/eslint.config.js
+++ tests/print-evaluated-rules/eslint.config.js
@@ -4,10 +4,5 @@ export default [
     ...javascriptConfig,
     {
         ignores: ['eslint.config.js']
-    },
-    {
-        files: ['*.js'],
-        rules: {
-        },
-    },
+    }
 ];
